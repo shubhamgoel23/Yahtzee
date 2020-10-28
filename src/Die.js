ce90751bd9c32870e06840f7f924c63ac580d227
Goel Shubham (RBEI/EAA2) <hgs9kor@bosch.com>
2020-10-28 14:20:21 +0530

    replacing text with dice icons

--- src/Die.js
+++ src/Die.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  static defaultProps = {
+    numberWords: ["one", "two", "three", "four", "five", "six"],
+    val: 5
+  };
 constructor(props){
   super(props);
   this.handleClick=this.handleClick.bind(this);
@@ -12,14 +16,11 @@ handleClick(){
 }
 
   render() {
+    const { numberWords, locked, val, disabled} = this.props;
+    let classes = `Die fas fa-dice-${numberWords[val - 1]} fa-5x `;
+    if(locked) classes += 'Die-locked '
     return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.handleClick}
-      >
-        {this.props.val}
-      </button>
+      <i className={classes} onClick={this.handleClick} disabled={disabled}/>
     );
   }
 }
