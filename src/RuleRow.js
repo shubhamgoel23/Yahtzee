063b8100d1f4c8b994c41d40faf994c65601766b
Goel Shubham (RBEI/EAA2) <hgs9kor@bosch.com>
2020-10-28 13:12:58 +0530

    preventing score reuse

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -3,10 +3,14 @@ import './RuleRow.css'
 
 class RuleRow extends Component {
   render() {
+    const {score, name, doScore} = this.props;
+    const disabled = score!==undefined;
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
+      <tr className={`RuleRow RuleRow-${!disabled ? "active" : "disabled"}`} 
+      onClick={!disabled ? this.props.doScore : null}
+      >
+        <td className="RuleRow-name">{name}</td>
+        <td className="RuleRow-score">{score}</td>
       </tr>
     )
   }
